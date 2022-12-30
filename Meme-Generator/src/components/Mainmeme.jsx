dd98e1c06be337da8fc3c7abb15f41c912dad035
gioshwua <reanokennethjoshua11@gmail.com>
2022-12-30 21:53:57 +0800

    added random image functionality

--- Meme-Generator/src/components/Mainmeme.jsx
+++ Meme-Generator/src/components/Mainmeme.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import MemeData from "../../MemeData";
 function Mainmeme() {
+  const [meme, memeSet] = React.useState("https://i.imgflip.com/1bil.jpg");
+
+  const changeMeme = () => {
+    const memeArr = MemeData.data.memes;
+    const randomNum = Math.floor(Math.random() * memeArr.length);
+    const randomUrl = memeArr[randomNum].url;
+    memeSet(randomUrl);
+  };
+
   return (
     <div className="container">
       <div className="inputs">
         <input placeholder="Top Text" />
         <input placeholder="Bottom Text" />
       </div>
-      <button className="changeMeme"> CHANGE MEME</button>
+      <button className="changeMeme" onClick={changeMeme}>
+        {" "}
+        CHANGE MEME
+      </button>
+      <img src={meme} />
     </div>
   );
 }
